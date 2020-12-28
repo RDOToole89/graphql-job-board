7961a799b8c258ccdf8789dc52375fa9f366a522
RDOToole89 <roibinotoole@gmail.com>
2020-12-28 23:24:14 +0100

    Remove hardcoded companyId, id now generated server side

--- client/src/JobForm.js
+++ client/src/JobForm.js
@@ -14,9 +14,9 @@ export class JobForm extends Component {
 
   handleClick(event) {
     event.preventDefault();
-    const companyId = 'HJRa-DOuG';
+    // const companyId = 'HJRa-DOuG';
     const { title, description } = this.state;
-    createJob({ title, description, companyId }).then((job) => {
+    createJob({ title, description }).then((job) => {
       this.props.history.push(`/jobs/${job.id}`);
     });
     console.log('should post a new job:', this.state);
